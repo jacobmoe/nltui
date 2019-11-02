d2fc36c59d618288609f446a1be354b316417490
Jacob Moeller <jacobmoe@gmail.com>
2019-11-02 19:41:19 -0400

    add page with title

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Corner, Direction, Layout};
+use tui::layout::{Alignment, Constraint, Corner, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
 use tui::widgets::{Block, Borders, List, SelectableList, Paragraph, Text, Widget};
 use tui::Terminal;
@@ -16,26 +16,44 @@ use unicode_width::UnicodeWidthStr;
 
 use crate::util::event::{Event, Events};
 
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
+struct Page{
+    page_type: String,
+    name: String,
+    items: Vec<Item>,
+    selected: Option<usize>,
+}
+
+impl Page{
+    pub fn new(page_type: String, name: String) -> Page {
+        Page{
+            page_type: page_type,
+            name: name,
+            items: Vec::new(),
+            selected: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Item {
     id: String,
     name: String,
-    items: Vec<Item>,
+    page: Page,
 }
 
 impl Item{
-    pub fn new(id: String, name: String) -> Item {
+    pub fn new(item_type: String, id: String, name: String) -> Item {
         Item{
             id: id,
-            name: name,
-            items: Vec::new(),
+            name: name.clone(),
+            page: Page::new(item_type.clone(), name.clone()),
         }
     }
 }
 
 struct App{
-    items: Vec<Item>,
-    selected: Option<usize>,
+    page: Page,
     item_primary_style: Style,
     item_secondary_style: Style,
     input: String,
@@ -44,16 +62,19 @@ struct App{
 impl App {
     fn new() -> App {
         let items = vec![
-            Item{id: String::from("item1id"), name: String::from("item1name"), items: Vec::new()},
-            Item{id: String::from("item2id"), name: String::from("item2name"), items: Vec::new()},
-            Item{id: String::from("item3id"), name: String::from("item3name"), items: Vec::new()},
-            Item{id: String::from("item4id"), name: String::from("item4name"), items: Vec::new()},
-            Item{id: String::from("item5id"), name: String::from("item5name"), items: Vec::new()},
+            Item{id: String::from("item1id"), name: String::from("item1name"), page: Page::new(String::from("item"), String::from("item1"))},
+            Item{id: String::from("item2id"), name: String::from("item2name"), page: Page::new(String::from("item"), String::from("item2"))},
+            Item{id: String::from("item3id"), name: String::from("item3name"), page: Page::new(String::from("item"), String::from("item3"))},
+            Item{id: String::from("item4id"), name: String::from("item4name"), page: Page::new(String::from("item"), String::from("item4"))},
+            Item{id: String::from("item5id"), name: String::from("item5name"), page: Page::new(String::from("item"), String::from("item5"))},
         ];
 
+        let mut page = Page::new(String::from("page type"), String::from("page name"));
+        page.items = items;
+        page.selected = Some(0);
+
         App{
-            items: items,
-            selected: None,
+            page: page,
             item_primary_style: Style::default().fg(Color::White),
             item_secondary_style: Style::default().fg(Color::Yellow),
             input: String::new(),
@@ -79,24 +100,54 @@ fn run(mut app: App) -> Result<(), failure::Error> {
 
     'main: loop {
         terminal.draw(|mut f| {
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+            let wrapper_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Percentage(5),
+                    Constraint::Percentage(95),
+                ].as_ref())
                 .split(f.size());
+
+            let block = Block::default()
+                .borders(Borders::LEFT | Borders::RIGHT)
+                .border_style(Style::default().fg(Color::White))
+                .style(Style::default().bg(Color::Black));
+
+            let title = format!("{}: {}", app.page.page_type, app.page.name);
+            Paragraph::new([Text::styled(title, Style::default().fg(Color::Red))].iter())
+                .block(block.clone())
+                .alignment(Alignment::Center)
+                .render(&mut f, wrapper_chunks[0]);
+
+            let primary_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([
+                    Constraint::Percentage(30),
+                    Constraint::Percentage(70),
+                ].as_ref())
+                .split(wrapper_chunks[1]);
+
+            let info_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(80),
+                ].as_ref())
+                .split(primary_chunks[1]);
 
             let style = Style::default().fg(Color::Black).bg(Color::White);
             SelectableList::default()
-                .block(Block::default().borders(Borders::ALL).title("List"))
-                .items(&app.items.iter().map(|i| { i.name.clone() }).collect::<Vec<_>>())
-                .select(app.selected)
+                .block(Block::default().borders(Borders::ALL).title("Menu"))
+                .items(&app.page.items.iter().map(|i| { i.name.clone() }).collect::<Vec<_>>())
+                .select(app.page.selected)
                 .style(style)
                 .highlight_style(style.fg(Color::LightGreen).modifier(Modifier::BOLD))
                 .highlight_symbol("=>")
-                .render(&mut f, chunks[0]);
+                .render(&mut f, primary_chunks[0]);
 
-            match app.selected {
+            match app.page.selected {
                 Some(index) => {
-                    let item = app.items[index].clone();
+                    let item = app.page.items[index].clone();
 
                     let fields = vec![
                         format!("Item ID: {}", item.id),
@@ -113,9 +164,9 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                     List::new(item_info)
                         .block(Block::default().borders(Borders::ALL).title("Selected"))
                         .start_corner(Corner::TopLeft)
-                        .render(&mut f, chunks[1]);
+                        .render(&mut f, info_chunks[0]);
 
-                    let item_list = app.items[index].items.iter().map(|i| {
+                    let item_list = app.page.items[index].page.items.iter().map(|i| {
                         Text::styled(
                             format!("{}", i.name),
                             app.item_secondary_style,
@@ -123,9 +174,9 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                     });
 
                     List::new(item_list)
-                        .block(Block::default().borders(Borders::ALL).title("Selected"))
-                        .start_corner(Corner::BottomLeft)
-                        .render(&mut f, chunks[1]);
+                        .block(Block::default().borders(Borders::ALL).title("Items"))
+                        .start_corner(Corner::TopLeft)
+                        .render(&mut f, info_chunks[1]);
                 }
                 None => {}
             }
@@ -141,11 +192,11 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                     break;
                 }
                 Key::Left => {
-                    app.selected = None;
+                    app.page.selected = None;
                 }
                 Key::Down => {
-                    app.selected = if let Some(selected) = app.selected {
-                        if selected >= app.items.len() - 1 {
+                    app.page.selected = if let Some(selected) = app.page.selected {
+                        if selected >= app.page.items.len() - 1 {
                             Some(0)
                         } else {
                             Some(selected + 1)
@@ -155,21 +206,21 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                     };
                 }
                 Key::Up => {
-                    app.selected = if let Some(selected) = app.selected {
+                    app.page.selected = if let Some(selected) = app.page.selected {
                         if selected > 0 {
                             Some(selected - 1)
                         } else {
-                            Some(app.items.len() - 1)
+                            Some(app.page.items.len() - 1)
                         }
                     } else {
                         Some(0)
                     }
                 }
                 Key::Char('\n') => {
-                    println!("{:?}", app.selected)
+                    println!("{:?}", app.page.selected)
                 }
                 Key::Char('a') => {
-                    println!("{:?}", app.selected);
+                    println!("{:?}", app.page.selected);
 
                     'input: loop {
                         terminal.draw(|mut f| {
@@ -183,9 +234,9 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                                 .block(Block::default().borders(Borders::ALL).title("Input"))
                                 .render(&mut f, chunks[0]);
 
-                            match app.selected {
+                            match app.page.selected {
                                 Some(index) => {
-                                    let list = app.items[index]
+                                    let list = app.page.items[index].page
                                         .items
                                         .iter()
                                         .enumerate()
@@ -212,17 +263,17 @@ fn run(mut app: App) -> Result<(), failure::Error> {
                                 Key::Ctrl('c') => {
                                     break 'main;
                                 }
-                                Key::Char('q') => {
+                                Key::Ctrl('s') => {
                                     break;
                                 }
                                 Key::Char('\n') => {
-                                    match app.selected {
+                                    match app.page.selected {
                                         Some(index) => {
                                             let input: String = app.input.drain(..).collect();
                                             let id = input.clone();
                                             let name = input.clone();
-                                            let item = Item::new(id, name);
-                                            app.items[index].items.push(item);
+                                            let item = Item::new(String::from("Item"), id, name);
+                                            app.page.items[index].page.items.push(item);
                                         }
                                         None => {}
                                     }

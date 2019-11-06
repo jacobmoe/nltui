5c054efe5a3492183fe17059a8b3ead3276a3bfd
Jacob Moeller <jacobmoe@gmail.com>
2019-11-06 08:47:14 -0500

    transform internal to external list

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ pub fn run(list: List, page_options: Vec<PageOptions>) -> Result<(), failure::Er
     let root = InternList::new(list.name);
     let mut app = App::new(root);
 
-    for item in items_from_supplied(&mut app, 0, &list.items) {
+    for item in items_from_user(&mut app, 0, &list.items) {
         app.lists[0].items.push(item);
     }
 
@@ -53,25 +53,25 @@ pub fn run(list: List, page_options: Vec<PageOptions>) -> Result<(), failure::Er
     app::run(app)
 }
 
-fn items_from_supplied(app: &mut App, current_list_index: usize, supplied_items: &Vec<Item>) -> Vec<InternItem> {
-    if supplied_items.len() == 0 {
+fn items_from_user(app: &mut App, current_list_index: usize, user_items: &Vec<Item>) -> Vec<InternItem> {
+    if user_items.len() == 0 {
         return Vec::new();
     }
 
-    supplied_items.iter().map(|supplied_item| {
+    user_items.iter().map(|user_item| {
         let mut item = InternItem::new(
-            supplied_item.id.clone(),
-            supplied_item.name.clone(),
+            user_item.id.clone(),
+            user_item.name.clone(),
         );
 
-        match &supplied_item.list {
-            Some(next_supplied_list) => {
-                let list_index = app.add_list(next_supplied_list.name.clone());
+        match &user_item.list {
+            Some(next_user_list) => {
+                let list_index = app.add_list(next_user_list.name.clone());
                 app.lists[list_index].previous = Some(current_list_index);
 
                 item.list_index = Some(list_index);
 
-                for next_item in items_from_supplied(app, list_index, &next_supplied_list.items) {
+                for next_item in items_from_user(app, list_index, &next_user_list.items) {
                     app.lists[list_index].items.push(next_item);
                 }
 
@@ -84,4 +84,36 @@ fn items_from_supplied(app: &mut App, current_list_index: usize, supplied_items:
 
         item
     }).collect()
+}
+
+fn items_to_user(app: &App, items: &Vec<InternItem>) -> Vec<Item> {
+    if items.len() == 0 {
+        return Vec::new();
+    }
+
+    return items.iter().map(|item| {
+        let mut user_item = Item::new(
+            item.id.clone(),
+            item.name.clone(),
+            None,
+        );
+
+        match item.list_index {
+            Some(index) => {
+                let mut next_user_list = List::new(
+                    app.lists[index].name.clone(),
+                    Vec::new(),
+                );
+
+                for next_user_item in items_to_user(app, &app.lists[index].items) {
+                    next_user_list.items.push(next_user_item);
+                }
+
+                user_item.list = Some(next_user_list);
+            }
+            None => {}
+        }
+
+        user_item
+    }).collect();
 }

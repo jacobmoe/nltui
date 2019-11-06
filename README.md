98b0c5fd829acaa126972ab386d37a6b6c5d5799
Jacob Moeller <jacobmoe@gmail.com>
2019-11-05 22:59:14 -0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
 # nltui
 
+A Text User Interface for managing arbitrarily deep nested lists in rust.
+
 ![edit](/docs/edit.png)
 ![add](/docs/add.png)
+
+## usage
+
+```rust
+extern crate nltui;
+
+fn main() -> Result<(), failure::Error> {
+    let list = nltui::List{
+        name: String::from("first list"),
+        items: vec![
+            nltui::Item{
+                id: String::from("item 1 for first list id"),
+                name: String::from("item 1 for first list name"),
+                list: Some(nltui::List{
+                    name: String::from("second list"),
+                    items: vec![
+                        nltui::Item{
+                            id: String::from("item 1 for second list id"),
+                            name: String::from("item 1 for second list name"),
+                            list: Some(nltui::List{
+                                name: String::from("third list"),
+                                items: vec![
+                                    nltui::Item{
+                                        id: String::from("item 1 for third list id"),
+                                        name: String::from("item 1 for third list name"),
+                                        list: None,
+                                    },
+                                ],
+                            }),
+                        },
+                    ],
+                }),
+            },
+            nltui::Item{
+                id: String::from("item 2 for first list id"),
+                name: String::from("item 2 for first list name"),
+                list: None,
+            },
+        ]
+    };
+
+    let mut page_options = vec![
+        nltui::PageOptions::new(String::from("Example1")),
+        nltui::PageOptions::new(String::from("Example2")),
+        nltui::PageOptions::new(String::from("Example3")),
+    ];
+
+    page_options[0].disable_delete = true;
+    page_options[2].disable_add = true;
+
+    nltui::run(list, page_options)
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
 extern crate nltui;
 
 fn main() -> Result<(), failure::Error> {
-    let nested_items = vec![
-        nltui::Item::new(String::from("nested item 1 id"), String::from("nested item 1 name"), None),
-        nltui::Item::new(String::from("nested item 2 id"), String::from("nested item 2 name"), None),
-        nltui::Item::new(String::from("nested item 3 id"), String::from("nested item 3 name"), None),
-        nltui::Item::new(String::from("nested item 4 id"), String::from("nested item 4 name"), None),
-        nltui::Item::new(String::from("nested item 5 id"), String::from("nested item 5 name"), None),
-    ];
-
-    let nested_list = nltui::List::new(String::from("second list"), nested_items);
-
-    let items = vec![
-        nltui::Item::new(String::from("item 1 id"), String::from("item 1 name"), Some(nested_list)),
-        nltui::Item::new(String::from("item 2 id"), String::from("item 2 name"), None),
-        nltui::Item::new(String::from("item 3 id"), String::from("item 3 name"), None),
-        nltui::Item::new(String::from("item 4 id"), String::from("item 4 name"), None),
-        nltui::Item::new(String::from("item 5 id"), String::from("item 5 name"), None),
-    ];
-
-    let list = nltui::List::new(String::from("first list"), items);
+    let list = nltui::List{
+        name: String::from("first list"),
+        items: vec![
+            nltui::Item{
+                id: String::from("item 1 for first list id"),
+                name: String::from("item 1 for first list name"),
+                list: Some(nltui::List{
+                    name: String::from("second list"),
+                    items: vec![
+                        nltui::Item{
+                            id: String::from("item 1 for second list id"),
+                            name: String::from("item 1 for second list name"),
+                            list: Some(nltui::List{
+                                name: String::from("third list"),
+                                items: vec![
+                                    nltui::Item{
+                                        id: String::from("item 1 for third list id"),
+                                        name: String::from("item 1 for third list name"),
+                                        list: None,
+                                    },
+                                ],
+                            }),
+                        },
+                    ],
+                }),
+            },
+            nltui::Item{
+                id: String::from("item 2 for first list id"),
+                name: String::from("item 2 for first list name"),
+                list: None,
+            },
+        ]
+    };
 
     let mut page_options = vec![
         nltui::PageOptions::new(String::from("Example1")),
         nltui::PageOptions::new(String::from("Example2")),
         nltui::PageOptions::new(String::from("Example3")),
     ];
+
     page_options[0].disable_delete = true;
     page_options[2].disable_add = true;
 
